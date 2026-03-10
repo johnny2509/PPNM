7025236e3ab445c31564e35c39dd715b2fbaa238
Jonathan-Peter Gärtner <jonathan-petergartner@MacBook-Pro-tilhrende-Jonathan-Peter.local>
2026-03-10 16:52:41 +0100

    WIP 2.2

--- exercises/input_output/main_2.cc
+++ exercises/input_output/main_2.cc
@@ -3,7 +3,10 @@
 #include <cmath>
 #include <string>
 
-double x;
-while( std::cin >> x ){
-	std::cout << x <<" "<< std::sin(x) <<" "<< std::cos(x) << std::endl;
+int main() {
+	double x;
+	while( std::cin >> x ){
+		std::cout << x <<" "<< std::sin(x) <<" "<< std::cos(x) << std::endl;
 	}
+	return 0;
+}
